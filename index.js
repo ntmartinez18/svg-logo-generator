b930ee2a9ff2f788ae4bf37ac3efc210658772d9
taylor.martinez <taylor.swot.2018@gmail.com>
2023-10-13 16:47:28 -0500

    created shape function

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const questions = [
         type: 'list',
         name: 'shape',
         message: 'Select a shape',
-        choices: ['Circle', 'Triangle', 'Square'],
+        choices: ['circle', 'triangle', 'square'],
     },
     {
         type: 'input',
@@ -33,13 +33,24 @@ const questions = [
 const generatedSVG = (answers) => {
     console.log(answers)
     const { text, textColor, shape, shapeColor } = answers;
+    let shapeContent =''
+    if (shape === 'circle') {
+        shapeContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}"/>`;
+    } else if (shape === 'triangle') {
+        shapeContent = `<polygon points="150, 20 275, 180 25, 180" fill="${shapeColor}"/>`;
+    } else if (shape === 'square') {
+        shapeContent = `<rect x="50" y="50" width="200" height="200" fill="${shapeColor}"/>`;
+    }
+
     const svgContent = `
-    ${text}
-    ${textColor}
-    ${shape}
-    ${shapeColor}`;
+    <svg width="300" height="200">
+    <text x="50%" y="50%" text-anchor="middle" fill="${textColor}">${text}</text>
+    ${shapeContent}
+    </svg>
+    `;
     return svgContent;
-};
+    };
+
 // return the generated content
 inquirer
 .prompt(questions)
